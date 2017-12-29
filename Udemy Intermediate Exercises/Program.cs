af9e72c5dc9d05b2e9e302fe3255f2a934977ee0
Hrvoje <Hrvoje@DESKTOP-DA867A7>
2017-12-29 01:35:12 +0100

    StopWatch

---
+++ Udemy Intermediate Exercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Udemy_Intermediate_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
