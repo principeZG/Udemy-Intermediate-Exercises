9929a47d3d9ae359baad3f7e27d759556c28fd50
Mr. H <hkraljevic@gmail.com>
2018-01-05 13:13:28 +0100

    Update DbCommand.cs

--- Exercise Polymorphism/DbCommand.cs
+++ Exercise Polymorphism/DbCommand.cs
@@ -1,4 +1,18 @@
-﻿using System;
+/*Design a class called DbCommand for executing an instruction against the database. A 
+DbCommand cannot be in a valid state without having a connection. So in the constructor of 
+this class, pass a DbConnection. Don’t forget to cater for the null.
+Each DbCommand should also have the instruction to be sent to the database. In case of SQL 
+Server, this instruction is expressed in T-SQL language. Use a string to represent this instruction. 
+Again, a command cannot be in a valid state without this instruction. So make sure to receive it 
+in the constructor and cater for the null reference or an empty string. 
+Each command should be executable. So we need to create a method called Execute(). In this 
+method, we need a simple implementation as follows: 
+Open the connection
+Run the instruction 
+Close the connection
+*/
+
+using System;
 
 namespace Exercise_Polymorphism
 {
